a4c7ea693bae03f5c0bfa396877fe421d2d96dfd
chaknogf <chaknogf@gmail.com>
2025-05-19 18:43:01 -0600

    feat: id

--- app/schemas/actividad.py
+++ app/schemas/actividad.py
@@ -30,7 +30,7 @@ class MetadatosActividad(BaseModel):
 
 
 class ActividadBase(BaseModel):
-    id: Optional[int] = None
+    id: int
     tema: str = field(...)
     actividad: int = field(...)
     servicio_encargado: str = field(...)
