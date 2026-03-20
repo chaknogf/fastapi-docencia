b81f50dcaac3ea9f1041a88f7be7005d17f28755
chaknogf <chaknogf@gmail.com>
2026-03-20 12:11:33 -0600

    debugg: main

--- main.py
+++ main.py
@@ -22,11 +22,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:4200",
-        "https://www.hosptecpan.space"
-    ],
-    allow_credentials=True,  # ✅ ahora sí es válido
+    allow_origins=["*"],
+    allow_credentials=True,  
     allow_methods=["*"],
     allow_headers=["*"],
 )
